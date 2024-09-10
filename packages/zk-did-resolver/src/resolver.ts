e200b613a693500a797e58d14133b747aa2a8c27
Meyanis95 <yanis.meziane@essec.edu>
2024-09-10 19:26:12 +0200

    feat: did resolver basic commands

--- packages/zk-did-resolver/src/resolver.ts
+++ packages/zk-did-resolver/src/resolver.ts
@@ -1,3 +1,10 @@
 // To do:
 // Build a resolver method for verification keys
 // Build a resolver method for JSON-LD
+
+export default function getDIDDocument(
+  DIDDocumentContext: string,
+  DID: string
+) {
+  // should return the corresponding DID document from `"id": "did:example:cdf:35LB7w9ueWbagPL94T9bMLtyXDj9pX5o"`
+}
